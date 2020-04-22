6efa64bf1a2c0e88dd4469baa4674070328062bd
moacirtorres <moacirtorress@gmail.com>
2020-04-22 16:53:29 -0300

    Commit inicial

---
+++ src/components/menu/menu.vue
@@ -0,0 +1,27 @@
+<template>
+<div>
+<nav>
+    <ul>
+        <li v-for="rota in rotas" :key="rota.path"><router-link :to="rota.path ? rota.path : '/'">{{ rota.titulo }}</router-link></li>
+    </ul>
+</nav>
+</div>
+</template>
+
+<script>
+
+export default {
+
+  props: {
+    rotas: {
+      type: Array,
+      required: true
+    }
+  }
+
+}
+
+</script>
+
+<style>
+</style>
---
+++ src/components/shared/botao.vue
@@ -0,0 +1,56 @@
+<template>
+<button @click="disparaAcao()" class="botao botao-perigo" :type="tipo">{{ rotulo }}</button>
+</template>
+
+<script>
+
+export default {
+  props: {
+    tipo: {
+      type: String,
+      required: true
+    },
+
+    rotulo: {
+      type: String,
+      required: true
+    },
+
+    confirmacao: Boolean,
+    estilo: String
+  },
+
+  methods: {
+
+    disparaAcao () {
+      if (this.confirmacao) {
+        if (confirm('Confirma operacao?')) {
+          this.$emit('botaoAtivado')
+        }
+        return
+      }
+      this.$emit('botaoAtivado')
+    }
+  }
+}
+</script>
+
+<style>
+    .botao {
+        display: inline-block;
+        padding: 10px;
+        border-radius: 3px;
+        margin: 10px;
+        font-size: 1.2em;
+    }
+
+    .botao-perigo {
+        background: firebrick;
+        color: white;
+    }
+
+    .botao-padrao {
+        background: darkcyan;
+        color: white;
+    }
+</style>
